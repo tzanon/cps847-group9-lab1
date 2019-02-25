6b9e4373b8c537d0676348cf5b221298dd85c1d8
Shiqi Dong <dong.shi.qi@hotmail.com>
2019-02-24 19:57:51 -0500

    Create cps-a2-test.py

---
+++ cps-a2-test.py
@@ -0,0 +1,11 @@
+# super simple program which calculates the area of any rectangle given any base and height as input
+
+width_in = input("Please input the width");
+height_in = input("Please input the height");
+
+width = (int)(width_in);
+height = (int)(height_in);
+
+area = width * height;
+
+print(area);
